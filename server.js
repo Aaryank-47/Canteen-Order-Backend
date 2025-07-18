7a475e1dbdf60e0cd8ff8488bd928b61d7c3dc13
aaryan <aaryankamalwanshi274@gmail.com>
2025-07-18 13:33:03 +0530

    getting error at tauri-app

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 
 //CORS
 const corsOptions = {
-  origin: [process.env.URL1, process.env.URL2, process.env.URL3, process.env.URL4],
+  // origin: [process.env.URL1, process.env.URL2, process.env.URL3, process.env.URL4],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   // allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
